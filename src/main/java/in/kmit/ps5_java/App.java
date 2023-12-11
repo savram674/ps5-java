971765951aab1d4526fda8f3bb00eb99c788bd86
savram674 <savitharamesh674@gmail.com>
2023-12-11 12:54:22 +0530

    maven java file

---
+++ src/main/java/in/kmit/ps5_java/App.java
@@ -0,0 +1,13 @@
+package in.kmit.ps5_java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to PS-5" );
+    }
+}
